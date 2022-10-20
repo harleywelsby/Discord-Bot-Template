7cd3643fc6d7bdec31afdae7557154614ae565f1
harleywelsby <harleywelsby9@gmail.com>
2022-10-21 08:11:05 +1300

    Working command register

--- commands/commandRegister.ts
+++ commands/commandRegister.ts
@@ -1,10 +1,26 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, Events } from 'discord.js';
+import { bot } from '../main.js';
+import { doExampleCommand } from './exampleCommand.js';
 
-// Command to be added to the commands list in main.ts, callable with /
+// Functions called by commands
+
+bot.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction as CommandInteraction;
+
+    switch (command.commandName) {
+        case 'examplecommand':
+            doExampleCommand(command);
+            break;
+        default:
+            command.reply(`I don't recognize that command ${command.user}`);
+    }
+});
+
+// Commands
+
 export const thingCommand = new SlashCommandBuilder()
-    .setName('cooltestcommand')
-    .setDescription('Say hello to the bot!')
-
-export const coolCommand = new SlashCommandBuilder()
-    .setName('coolertestcommand')
+    .setName('examplecommand')
     .setDescription('Say hello to the bot!')
--- commands/exampleCommand.ts
+++ commands/exampleCommand.ts
@@ -1,13 +1,7 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
 
-// Command to be added to the commands list in main.ts, callable with /
-export const exampleCommand = new SlashCommandBuilder()
-    .setName('examplecommand')
-    .setDescription('Say hello to the bot!')
-
 // Function to call when command is entered in Discord
-export const doCommand = (interaction: CommandInteraction) => {
+export const doExampleCommand = (interaction: CommandInteraction) => {
     const author = interaction.member.user;
     interaction.reply(`Hello ${author}!`);
 } 
