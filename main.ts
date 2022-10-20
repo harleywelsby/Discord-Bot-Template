39bc4ffebe9ad78cc1114486d2b3da02fcd321da
harleywelsby <harleywelsby9@gmail.com>
2022-10-20 21:33:19 +1300

    JSON import as module & dynamic file import testing

--- main.ts
+++ main.ts
@@ -4,29 +4,42 @@
 // || AUTHOR: Harley Welsby, https://github.com/harleywelsby  ||
 // =============================================================
 
-import { ActivityType, Client, IntentsBitField } from 'discord.js';
+import { ActivityType, Client, Events, IntentsBitField } from 'discord.js';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
-import { createRequire } from 'module';
-import { doCommand, exampleCommand } from './commands/exampleCommand.js';
+// import { createRequire } from 'module';
+import config from './config/config.json' assert { type: 'json' };
+import fs from 'node:fs';
 
-// Load the config json from ./config
-const require = createRequire(import.meta.url);
-export const config: JSON = require('../config/config.json');
+// Load the config json
+//const require: NodeRequire = createRequire(import.meta.url);
+// export const config: JSON = require('../config/config.json');
 
 // Load commands into Discord.js
-const commands = [];
-commands.push(exampleCommand.toJSON());
+// See https://discordjs.guide/creating-your-bot/command-deployment.html#guild-commands
+const commands: string[] = [];
+const commandFiles: string[] = fs.readdirSync('./commands').filter(file => file.endsWith('.js') || file.endsWith('.ts'));
+for (const file of commandFiles) {
+    let command: string = file;
+    if (file.endsWith('.ts')) {
+        command = file.slice(0, -3) + '.js';
+    }
+    // commands.push(command.data.toJSON());
+}
 
 // Refresh slash commands on startup
-const rest = new REST({ version: '9' }).setToken(config["Token"]);
+const rest = new REST({ version: '10' }).setToken(config.Token);
 (async () => {
     try {
-        console.log('Refreshing Slash Commands');
-        await rest.put(
-            Routes.applicationGuildCommands(config["ClientId"], config["GuildId"]),
+        console.log(`Refreshing ${commands.length} Slash Commands`);
+        
+        const data = await rest.put(
+            Routes.applicationCommands(config.ClientId),
             { body: commands }
         );
+
+        // console.log(typeof data);
+        // console.log(`${data.length} slash commands refreshed`);
     }
     catch (error) {
         console.error(error);
@@ -42,7 +55,7 @@ export const bot = new Client({ intents: [
 });
 
 // Login
-bot.on('ready', () => {
+bot.on(Events.ClientReady, () => {
     console.log('logged in!');
     
     // Custom activity - displays in the members side bar in the server
@@ -50,15 +63,15 @@ bot.on('ready', () => {
 });
 
 // Check for commands as they happen
-bot.on('interactionCreate', interaction => {
-    if (!interaction.isCommand()) return;
+bot.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isChatInputCommand()) return;
 
     switch (interaction.commandName) {
         case 'ExampleCommand':
-            doCommand(interaction);
+            // doCommand(interaction);
         default:
             break;
     }
 });
 
-bot.login(config["Token"]);
+bot.login(config.Token);
